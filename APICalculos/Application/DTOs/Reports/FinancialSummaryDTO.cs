10f6bee54a2995672ac74d86219c2b073670677a
VAAB <victor_15_avila@hotmail.es>
2026-02-07 02:02:42 -0300

    Actualizacion report summary

--- APICalculos/Application/DTOs/Reports/FinancialSummaryDTO.cs
+++ APICalculos/Application/DTOs/Reports/FinancialSummaryDTO.cs
@@ -5,6 +5,6 @@
         public decimal TotalVentas { get; set; }
         public decimal TotalPagosColaboradores { get; set; }
         public decimal TotalGastos { get; set; }
-        public decimal GananciaNeta => TotalVentas - (TotalPagosColaboradores + TotalGastos);
+        public decimal GananciaNeta => TotalVentas -  TotalGastos;
     }
 }
